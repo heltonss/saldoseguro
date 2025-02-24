aca64bee02a4855649221e1b5858e9d536b0e22a
heltonss <helton@heltonsouza.com.br>
2025-02-24 07:35:07 -0300

    chore(workflow): adiciona workflow para publicacao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'saldoseguro + projeto impacta + angular';
-  teste = 'teste';
+  title = 'saldoseguro + projeto impacta devops + angular';
 }
